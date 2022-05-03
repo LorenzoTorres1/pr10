af6913d099ec9b8ea02265695ebfb89c4d32295a
Raúl Enrique González Bondarchuk <96430972+raulbondarchuk@users.noreply.github.com>
2022-05-03 18:31:45 +0200

    Raul_Modificar

--- Lorenzo_Raul/Form1.Designer.cs
+++ Lorenzo_Raul/Form1.Designer.cs
@@ -29,23 +29,13 @@ namespace Lorenzo_Raul
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.Raúl = new System.Windows.Forms.CheckedListBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(100, 65);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(189, 23);
-            this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
@@ -53,13 +43,6 @@ namespace Lorenzo_Raul
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(184, 23);
             this.textBox2.TabIndex = 1;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(429, 65);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(186, 23);
-            this.textBox3.TabIndex = 2;
             // 
             // label1
             // 
@@ -107,29 +90,17 @@ namespace Lorenzo_Raul
             this.label4.TabIndex = 7;
             this.label4.Text = "Lista Socios";
             // 
-            // Raúl
-            // 
-            this.Raúl.FormattingEnabled = true;
-            this.Raúl.Location = new System.Drawing.Point(29, 166);
-            this.Raúl.Name = "Raúl";
-            this.Raúl.Size = new System.Drawing.Size(962, 256);
-            this.Raúl.TabIndex = 8;
-            this.Raúl.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1032, 452);
-            this.Controls.Add(this.Raúl);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -138,16 +109,12 @@ namespace Lorenzo_Raul
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.CheckedListBox Raúl;
     }
 }
 
